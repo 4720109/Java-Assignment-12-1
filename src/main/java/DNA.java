d62cee75b197ed5af9caa7fa7d06be825d229bba
4720109 <103705056+4720109@users.noreply.github.com>
2022-07-08 11:08:15 +0900

    Update DNA.java

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,3 +1,25 @@
-public class DNA {
 
+/**
+ * クラス DNA の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
+public class DNA {
+    public String str="";
+    
+    public String getPair(){
+        String pairStr="";
+        String pairCh="";
+        for( int i=0; i < this.str.length(); i++) {
+            if (str.charAt(i) == 'T') pairCh="T";
+            else if(str.charAt(i)=='A') pairCh="A";
+            else if (str.charAt(i)=='G') pairCh="C";
+            else if(str.charAt(i)=='C') pairCh="G";
+            pairStr+=pairCh;
+        }
+     
+        return pairStr;
+    }
 }
+
